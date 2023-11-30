a4dd8f8b9b9833b9503c391a611aed61a1ea54ae
pabon98 <pabon@orangetoolz.com>
2023-12-01 00:27:09 +0600

    project set up done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navbar from './Components/Navbar'
+import Login from './Components/Login'
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+       <Login/> 
+    </>
+  )
+}
+
+export default App
