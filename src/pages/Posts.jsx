6ffe2951890d792fe8fa4cb22fc83a70a956282c
pabon98 <pabon@orangetoolz.com>
2023-12-06 01:37:36 +0600

    loader added in posts page

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -4,29 +4,40 @@ import Post from '../Components/Post';
 
 const Posts = () => {
   const [posts, setPosts] = useState([])
+  const [loading, setLoading] = useState(true)
+  
 
   useEffect(()=>{
  fetch('https://jsonplaceholder.typicode.com/posts')
 .then(res => res.json())
-.then(data=> setPosts(data));
+.then((data)=>{
+  setPosts(data);
+setLoading(false)
+})
   }, [])
   // console.log(posts)
 
   return (
     <div className='container'>
-    <h1 className='text-center'>Total Posts {posts.length}</h1>
-    <div className="row">
-      {
-        posts.map((post)=>(
-          <Post
-          key={post.id}
-          post={post}
-          ></Post>
-        )
-         
-        )
-      }
-    </div>
+   {
+    loading ? <div className="spinner-border" role="status">
+    <span className="visually-hidden">Loading...</span>
+  </div> : 
+   <div className="row">
+    <h2 className='text-center'>Total Posts {posts.length}</h2>
+   {
+     posts.map((post)=>(
+       <Post
+       key={post.id}
+       post={post}
+       ></Post>
+     )
+      
+     )
+   }
+ </div>
+   }
+   
   </div>
   )
 }
